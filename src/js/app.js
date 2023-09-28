6efac016d353e8cee8807b1e03d83b19d434da43
Zunickx <Zunickx@gmail.com>
2023-09-28 23:27:36 +0300

    Create file app.js, basic.js

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */ /* - отключение правила ESLint, которое запрещает объявление неиспользуемых переменных*/ 
+/* eslint-disable no-console */ /*отключение правила ESLint, которое запрещает использование консольных команд */
+/* eslint-disable max-classes-per-file */ /*отключение правила ESLint, которое ограничивает количество классов в одном файле */
+
+impot Character from './basic';
---
+++ src/js/basic.js
@@ -0,0 +1,26 @@
+/* eslint-disable max-classes-per-file */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-console */
+
+export default class Character {
+    constructor (name, type) {
+        if(name.length < 2 || name.length > 10) throw new Error ('Имя персонажа должно быть не менее 2 символов и не более 10 символов')
+        else this.name = name;
+    }
+
+    class CharacterType {
+        constructor(characterType, atack, defence) {
+            this.characterType = characterType;
+            this.atack = atack;
+            this.defence = defence;
+        }
+    }
+
+    const characterType = [];
+    characterType.push(new CharacterType('Bowerman', 25, 25));
+    characterType.push(new CharacterType('Swordsman', 40, 10));
+    characterType.push(new CharacterType('Magician', 10, 40));
+    characterType.push(new CharacterType('Undead', 25, 25));
+    characterType.push(new CharacterType('Zombie', 40, 10));
+    characterType.push(new CharacterType('Daemon', 10, 40));
+}
