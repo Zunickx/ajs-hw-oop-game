c0e432eedf9954201ca3c348154b21579c98d66b
Zunickx <Zunickx@gmail.com>
2023-09-29 23:09:42 +0300

    Change file basic.js

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,31 @@
 /* eslint-disable max-classes-per-file */ /*отключение правила ESLint, которое ограничивает количество классов в одном файле */
 
 impot Character from './basic';
+
+export class Bowerman extends Character {
+}
+
+export class Swordsman extends Character {
+}
+
+export class Magician extends Character {
+}
+
+export class Daemon extends Character {
+}
+
+export class Undead extends Character {
+}
+
+export class Zombie extends Character {
+}
+
+const bowerman = new Bowerman('Bow Man', 'Bowerman');
+const swordsman = new Swordsman('Swords Man', 'Swordsman');
+const magician = new Magician('MagiciaN', 'Magician');
+const daemon = new Daemon('DaemoN', 'Daemon');
+const undead = new Undead('UndeaD', 'Undead');
+const zombie = new Zombie('ZombiE', 'Zombie');
+
+console.log(Character.prototype);
+console.log(Character.prototype.name);
