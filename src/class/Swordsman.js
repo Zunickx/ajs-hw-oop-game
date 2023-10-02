d131577bf321ed1df98ec7d47940b17e75878e28
Zunickx <Zunickx@gmail.com>
2023-10-02 22:52:31 +0300

    Change file basic.js, create files class: Bowerman, Swordsman, Magician, Undead, Zombie, Daemon

---
+++ src/class/Swordsman.js
@@ -0,0 +1,9 @@
+import Character from "../js/basic";
+
+export default class Swordsman extends Character {
+    constructor(name, type = "Swordman", attack = 40, defence = 10) {
+        super(name, type);
+        this.attack = attack;
+        this.defence = defence;
+    }
+}
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,38 +1,48 @@
-/* eslint-disable max-classes-per-file */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable no-console */
-
 export default class Character {
     constructor (name, type) {
-        if(name.length < 2 || name.length > 10) throw new Error ('Имя персонажа должно быть не менее 2 символов и не более 10 символов')
-        else this.name = name;
+        const types = ['Bowerman', 'Swordsman', 'Magician', 'Undead', 'Zombie', 'Daemon'];
 
-        class CharacterType {
-            constructor(characterType, atack, defence) {
-                this.characterType = characterType;
-                this.atack = atack;
-                this.defence = defence;
-            }
+        if(name.length < 2 || name.length > 10) {
+            throw new Error ('Имя персонажа должно быть не менее 2 символов и не более 10 символов')
+        } else {
+            this.name = name;
         }
-    
-        const characterTypes = [];
-        characterTypes.push(new CharacterType('Bowerman', 25, 25));
-        characterTypes.push(new CharacterType('Swordsman', 40, 10));
-        characterTypes.push(new CharacterType('Magician', 10, 40));
-        characterTypes.push(new CharacterType('Undead', 25, 25));
-        characterTypes.push(new CharacterType('Zombie', 40, 10));
-        characterTypes.push(new CharacterType('Daemon', 10, 40));
-    
-        const characterType = characterTypes.find((el) => el.characterType === type);
-        if(!characterType) throw new Error ('Неверный тип персонажа')
-        else {
+
+        if(!types.includes(type)) {
+            throw new Error ('Неизвестный тип персонажа')
+        } else {
             this.type = type;
-            this.atack = characterType.atack;
-            this.defence = characterType.defence;
+            this.health = health;
+            this.level = level;
+            this.attack = attack;
+            this.defence = defence;
         }
+
+
+
+        // class CharacterType {
+        //     constructor(characterType, atack, defence) {
+        //         this.characterType = characterType;
+        //         this.atack = atack;
+        //         this.defence = defence;
+        //     }
+        // }
     
-        this.health = 100;
-        this.level = 1;
+        // const characterTypes = [];
+        // characterTypes.push(new CharacterType('Bowerman', 25, 25));
+        // characterTypes.push(new CharacterType('Swordsman', 40, 10));
+        // characterTypes.push(new CharacterType('Magician', 10, 40));
+        // characterTypes.push(new CharacterType('Undead', 25, 25));
+        // characterTypes.push(new CharacterType('Zombie', 40, 10));
+        // characterTypes.push(new CharacterType('Daemon', 10, 40));
+    
+        // const characterType = characterTypes.find((el) => el.characterType === type);
+        // if(!characterType) throw new Error ('Неверный тип персонажа')
+        // else {
+        //     this.type = type;
+        //     this.atack = characterType.atack;
+        //     this.defence = characterType.defence;
+        // }
     }    
 
     levelUp() {
