c1cf11e54a9457c6d852798d39f991374bceaf8e
Zunickx <Zunickx@gmail.com>
2023-10-03 22:15:43 +0300

    Create file undead.test.js

---
+++ src/js/__test__/undead.test.js
@@ -0,0 +1,35 @@
+import Undead from '../undead';
+
+// проверим тестом, что при создании объекта Undead задаются верные имя и ожидаемые значения
+
+test('test class undead with valid value of name', () => {
+  const undead = new Undead('undead');
+  const expected = {
+    name: 'undead',
+    type: 'Undead',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defence: 25,
+  };
+
+  expect(undead).toEqual(expected);
+});
+
+// проверим, что при создании имени класса, имя класса соответствует параметру от 2 до 10 символов
+
+test('test class undead with invalid value of name', () => {
+  expect(() => {
+    const undead = new Undead('zick');
+    return undead;
+  }).toThrow(new Error('Name length must be in [2;10] interval!'));
+});
+
+// проверим класс на соответствие типу
+
+test('test class undead with invalid value of type', () => {
+  expect(() => {
+    const undead = new Undead('undead', 'Pacman');
+    return undead;
+  }).toThrow(new Error('Not valid character type!'));
+});
